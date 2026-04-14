2dde8d07fef0e79b9739fb8f759ae0de1304dcfc
Rianne Boumans <rianne.boumans@zuyd.nl>
2026-04-14 23:11:37 +0200

    Add TypeScript declarations for CSS imports
    - Create global.d.ts to declare CSS/SCSS module types
    - Fixes TypeScript error: 'Cannot find module for side-effect import of ./globals.css'
    - Includes Next.js type references

---
+++ Code/src/global.d.ts
@@ -0,0 +1,7 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// CSS Module declarations
+declare module "*.css";
+declare module "*.scss";
+declare module "*.sass";
