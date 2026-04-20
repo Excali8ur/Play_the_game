f30f4885b1e78078d483b8095cc181b18298987d
Excali8ur <rianne.boumans@zuyd.nl>
2026-04-20 22:28:00 +0200

    Deleted all code. Starting over ;)

--- Code/src/global.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// CSS Module declarations
-declare module "*.css";
-declare module "*.scss";
-declare module "*.sass";
